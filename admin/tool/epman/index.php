ab2a3049b16bd73d54fac1c2a0249e22e7b92623
Paul Wolneykien <manowar@altlinux.org>
2014-03-04 16:12:20 +0400

    Add the epman module stub

---
+++ admin/tool/epman/index.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The front page of the educational process management module.
+ *
+ * @package    tool
+ * @subpackage epman
+ * @copyright  2014 Paul Wolneykien <manowar@altlinux.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require('../../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+admin_externalpage_setup('toolepman');
+$PAGE->set_pagelayout('maintenance');
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('pluginname', 'tool_epman'));
+echo $OUTPUT->footer();
