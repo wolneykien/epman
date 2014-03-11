a0981ff9a4a4b97d5650d7ed817b35fdcc5ef15f
Paul Wolneykien <manowar@altlinux.org>
2014-03-11 12:58:14 +0400

    The DB schema successfully installed

--- admin/tool/epman/version.php
+++ admin/tool/epman/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014030608; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014031101; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011092100; // Requires this Moodle version
 $plugin->component = 'tool_epman'; // Full name of the plugin (used for diagnostics)
 ?>
