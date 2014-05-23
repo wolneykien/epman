5fba92f8f75e46b11781bf96c9dd5dcdb73b20eb
Paul Wolneykien <manowar@altlinux.org>
2014-05-23 18:35:15 +0400

    Fill-up only the first-level parameters leaving the deeper ones untouched in order to support the PATCH requests properly

--- admin/tool/epman/crud/base.php
+++ admin/tool/epman/crud/base.php
@@ -36,7 +36,9 @@ class crud_external_api extends external_api {
   public static function validate_parameters(external_description $description, $params) {
     $newparams = self::cleanup_parameters($description, $params);
     $validated = parent::validate_parameters($description, $newparams);
-    return self::fillup_parameters($description, $validated);
+    self::fillup_parameters($description, $validated);
+
+    return $validated;
   }
 
   protected static function cleanup_parameters(external_description $description, $params) {
@@ -69,28 +71,13 @@ class crud_external_api extends external_api {
   }
 
   protected static function fillup_parameters(external_description $description, $params) {
-    if (is_null($params)) {
-      return null;
-    }
     if ($description instanceof external_single_structure) {
-      $newparams = array();
       foreach ($description->keys as $key => $val_desc) {
         if (!array_key_exists($key, $params)) {
-          $newparams[$key] = null;
-        } else {
-          $newparams[$key] = self::fillup_parameters($description->keys[$key], $params[$key]);
+          $params[$key] = null;
         }
       }
-    } elseif ($description instanceof external_multiple_structure) {
-      $newparams = array();
-      foreach ($params as $param) {
-        $newparams[] = self::fillup_parameters($description->content, $param);
-      }
-    } else {
-      $newparams = $params;
     }
-
-    return $newparams;
   }
 
 }
